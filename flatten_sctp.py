f159581604a6d749c93320a3a0e98969994ce7c2
yousef <y.elouam@gmail.com>
2020-08-24 18:04:54 +0200

    Add files via upload

--- flatten_sctp.py
+++ flatten_sctp.py
@@ -1,35 +1,38 @@
-#!/usr/bin/python
-
-"""
-(C) Copyright 2016-2017 Holger Hans Peter Freyther
-
-GNU AGPLv3+ 
-"""
-
 from scapy.all import rdpcap, Ether, IP, SCTP, SCTPChunkData, wrpcap
 import sys
+import os.path
 
-inp_fn = sys.argv[1] if len(sys.argv) > 1 else "input.pcap"
-out_fn = sys.argv[2] if len(sys.argv) > 2 else "output.pcap"
+def flatten_sctp(file_name):
+    pcap = rdpcap(file_name)
+    packets = []
 
-pcap = rdpcap(inp_fn)
-pkts = []
+    i = 0
+    seq = 0
 
-i = 0
-seq = 0
-for pkt in pcap:
-    ip = pkt['IP']
-    layer = ip.payload
-    while layer.name != 'NoPayload':
-        if layer.name == 'SCTP':
-            sport = layer.sport
-            dport = layer.dport
-            tag = layer.tag
-        if layer.name == 'SCTPChunkData':
-            # re-create the chunkdata as I don't find the routine to just have this data...
-            pkts.append(Ether()/IP()/SCTP(sport=sport,dport=dport,tag=tag)/SCTPChunkData(reserved=0, delay_sack=0, unordered=0, beginning=1, ending=1, stream_id=layer.stream_id, proto_id=layer.proto_id, stream_seq=layer.stream_seq, tsn=layer.tsn, data=layer.data))
-            seq = seq + 1
-        layer = layer.payload
-    i = i + 1
+    for pkt in pcap:
+        ip = pkt['IP']
+        layer = ip.payload
+        while layer.name != 'NoPayload':
+            if layer.name == 'SCTP':
+                sport = layer.sport
+                dport = layer.dport
+                tag = layer.tag
+            if layer.name == 'SCTPChunkData':
+                # re-create the chunkdata as I don't find the routine to just have this data...
+                packets.append(Ether()/IP()/SCTP(sport=sport,dport=dport,tag=tag)/SCTPChunkData(reserved=0, delay_sack=0, unordered=0, beginning=1, ending=1, stream_id=layer.stream_id, proto_id=layer.proto_id, stream_seq=layer.stream_seq, tsn=layer.tsn, data=layer.data))
+                seq = seq + 1
+            layer = layer.payload
+        i = i + 1
+    
+    extension = os.path.splitext(file_name)[1]
+    file_name = os.path.splitext(file_name)[0]
+    
+    wrpcap(file_name + '_out'+ extension, packets)
+    print("your file was processed successfuly, new file generated under {}_out{}".format(file_name, extension))
 
-wrpcap(out_fn, pkts)
+if __name__ == '__main__':
+    inp_fn = sys.argv[1]
+    flatten_sctp(inp_fn)
+    sys.exit(0)
+
+
